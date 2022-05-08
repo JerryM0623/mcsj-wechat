a5f0745fee1a1f62b4391dd6b9149249e048ab3b
熊熊熊子路 <1711279251@qq.com>
2022-05-09 04:06:54 +0800

    feat: 实现订单列表的签收、申请退款和删除订单操作

--- apis/order.api.js
+++ apis/order.api.js
@@ -5,7 +5,9 @@ const orderApis = {
     changeOrderStatusToPaid: BaseUrl + '/order/paid',
     getOrderList: BaseUrl + '/order/list',
     getOrderDetailInfo: BaseUrl + '/order/detail',
+    changeOrderStatusToReceive: BaseUrl + '/order/receive',
+    changeOrderStatusToRequestRefund: BaseUrl + '/order/refund',
+    deleteOrder: BaseUrl + '/order/delete'
 }
-
 
 module.exports = orderApis
--- pages/orderList/orderList.js
+++ pages/orderList/orderList.js
@@ -112,5 +112,116 @@ Page({
                 url: '/pages/showOrderInfo/showOrderInfo?orderId='+orderId
             })
         }
+    },
+    /**
+     * 将订单转换为已签收状态
+     */
+    changeOrderStatusToReceive(event){
+        const self = this;
+        const { orderId } = event.currentTarget.dataset;
+        if (orderId === undefined || orderId === null || orderId === ''){
+            showToastUtil.showToastFail('参数错误');
+        }else{
+            // 发送请求更新状态
+            wx.request({
+                url: orderApis.changeOrderStatusToReceive,
+                data: { orderId },
+                method: 'POST',
+                success: (res) => {
+                    const data = res.data;
+                    if (data.code !== 10051){
+                        showToastUtil.showToastNoIcon(data.msg)
+                    }else{
+                        showToastUtil.showToastNoIcon(data.msg);
+                        setTimeout(() => {
+                            self.getOrderList(self.data.userId);
+                        }, 1000)
+                    }
+                },
+                fail: () => {
+                    showToastUtil.showToastFail('操作失败');
+                },
+            })
+        }
+    },
+    /**
+     * 将订单转换为申请退款阶段
+     */
+    changeOrderStatusToRequestRefund(event){
+        const self = this;
+        wx.showModal({
+            title: '警告',
+            content: '你确定要申请退款吗?',
+            confirmColor: '#cb141d',
+            success (res) {
+                if (res.confirm) {
+                    const { orderId } = event.currentTarget.dataset;
+                    if (orderId === undefined || orderId === null || orderId === ''){
+                        showToastUtil.showToastFail('参数错误');
+                    }else{
+                        // 发送请求更新状态
+                        wx.request({
+                            url: orderApis.changeOrderStatusToRequestRefund,
+                            data: { orderId },
+                            method: 'POST',
+                            success: (res) => {
+                                const data = res.data;
+                                if (data.code !== 10054){
+                                    showToastUtil.showToastNoIcon(data.msg);
+                                }else{
+                                    showToastUtil.showToastNoIcon(data.msg);
+                                    setTimeout(() => {
+                                        self.getOrderList(self.data.userId);
+                                    }, 1000);
+                                }
+                            },
+                            fail: () => {
+                                showToastUtil.showToastFail('操作失败');
+                            },
+                        })
+                    }
+                }
+            }
+        })
+    },
+    /**
+     * 删除订单记录
+     */
+    deleteOrder(event){
+        const self = this;
+        wx.showModal({
+            title: '提示',
+            content: '这是一个模态弹窗',
+            confirmColor: '#cb141d',
+            success (res) {
+                if (res.confirm) {
+                    const { orderId } = event.currentTarget.dataset;
+                    if (orderId === undefined || orderId === null || orderId === ''){
+                        showToastUtil.showToastFail('参数错误');
+                    }else{
+                        // 发送请求更新状态
+                        wx.request({
+                            url: orderApis.deleteOrder,
+                            data: { orderId },
+                            method: 'POST',
+                            success: (res) => {
+                                const data = res.data;
+                                if (data.code !== 10057){
+                                    showToastUtil.showToastNoIcon(data.msg);
+                                }else{
+                                    showToastUtil.showToastNoIcon(data.msg);
+                                    setTimeout(() => {
+                                        self.getOrderList(self.data.userId);
+                                    }, 1000)
+                                }
+                            },
+                            fail: () => {
+                                showToastUtil.showToastFail('操作失败');
+                            },
+                        })
+                    }
+                }
+            }
+        })
     }
 })
